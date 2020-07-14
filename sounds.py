6b1ab59f13cf6ebd5b36fabce8fbcab84f31d33a
Supraja9726 <supraja.b.2015.cse@rajalakshmi.edu.in>
2020-07-14 20:34:18 +0530

    Speech emotion recognition along with data sets provided

--- sounds.py
+++ sounds.py
@@ -2,7 +2,7 @@ import librosa
 import soundfile
 import os, glob, pickle
 import numpy as np
-from sklearn.model_selection import train_test_splt
+from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
@@ -15,7 +15,7 @@ def extract_feature(file_name, mfcc, chroma, mel):
             stft=np.abs(librosa.stft(X))
         result=np.array([])
         if mfcc:
-            mfcc=np.mean(libros.feature.mfcc(y=X, sr=sample_rate,n_mfcc=40).T, axis=0)
+            mfcc=np.mean(librosa.feature.mfcc(y=X, sr=sample_rate,n_mfcc=40).T, axis=0)
         if chroma:
             chroma=np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
             result=np.hstack((result, chroma))
@@ -47,7 +47,7 @@ def load_data(test_size=0.2):
     for file in glob.glob("C:\\Users\\VC\\Downloads\\speech-emotion-recognition\\speech-emotion-recognition-ravdess-data\\Actor_*\\*.wav"):
         file_name=os.path.basename(file)
         emotion=emotions[file_name.split("-")[2]]
-        if emotion not in observed in emotions:
+        if emotion not in observed_emotions:
             continue
         feature=extract_feature(file, mfcc=True, chroma=True, mel=True)
         x.append(feature)
@@ -55,7 +55,7 @@ def load_data(test_size=0.2):
     return train_test_split(np.array(x), y, test_size=test_size, random_state=9)
 
 #Split the dataset
-x_train,_test,y_train,y_test=load_data(test_size=0.25)
+x_train,x_test,y_train,y_test=load_data(test_size=0.25)
 
 #Get the shape of the training and testing data sets
 print((x_train.shape[0],x_test.shape[0]))
@@ -64,7 +64,7 @@ print((x_train.shape[0],x_test.shape[0]))
 print(f'Features extracted: {x_train.shape[1]}')
 
 #Initializing multilayer Perceptron classifier
-model=MPCLassifier(alpha=0.01,batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500)
+model=MLPClassifier(alpha=0.01,batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500)
 
 #Train the model
 model.fit(x_train,y_train)
@@ -76,7 +76,7 @@ y_pred=model.predict(x_test)
 accuracy=accuracy_score(y_true=y_test, y_pred=y_pred)
 
 #Printing accuracy
-print("Accuracy: {:.2f%}".format(accuracy*100))
+print("Accuracy: {:.2f}%".format(accuracy*100))
 
             
         
