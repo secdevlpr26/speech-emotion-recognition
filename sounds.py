3da8743a6808852076850c16e2387afc1b9c79c9
Supraja9726 <supraja.b.2015.cse@rajalakshmi.edu.in>
2020-07-15 16:18:21 +0530

    Determines extracted features and its accuracy

--- sounds.py
+++ sounds.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
-#Extrac features (mfcc, chroma, mel) from a sound file
+#Extract features (mfcc, chroma, mel) from a sound file
 def extract_feature(file_name, mfcc, chroma, mel):
     with soundfile.SoundFile(file_name) as sound_file:
         X = sound_file.read(dtype="float32")
@@ -53,7 +53,10 @@ def load_data(test_size=0.2):
         feature=extract_feature(file, mfcc=True, chroma=True, mel=True)
         x.append(feature)
         y.append(emotion)
+    #prints all the features and emotions    
+    print(x,y)    
     return train_test_split(np.array(x), y, test_size=test_size, random_state=9)
+    
 
 #Split the dataset
 x_train,x_test,y_train,y_test=load_data(test_size=0.25)
@@ -64,8 +67,8 @@ print((x_train.shape[0],x_test.shape[0]))
 #Number of features extracted - count
 print(f'Features extracted: {x_train.shape[1]}')
 
-#Initializing multilayer Perceptron classifier
-model=MLPClassifier(alpha=0.01,batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500)
+#Initializing multilayer Perceptron classifier - set the max iteration based on achieving the convergence point
+model=MLPClassifier(alpha=0.01,batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=1000)
 
 #Train the model
 model.fit(x_train,y_train)
