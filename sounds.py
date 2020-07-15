380c4c7cdc6e3c20cf8e3e3f468931c4c4af007c
Supraja9726 <supraja.b.2015.cse@rajalakshmi.edu.in>
2020-07-15 10:04:00 +0530

    Determines extracted features and its accuracy

--- sounds.py
+++ sounds.py
@@ -2,6 +2,7 @@ import librosa
 import soundfile
 import os, glob, pickle
 import numpy as np
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
